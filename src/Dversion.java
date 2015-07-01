a331a0f3c78b8a40581dd0f62674538046250f43
ycbbcy <ycbmyjay@gmail.com>
2015-07-01 19:50:47 +0800

    Change-Id: I29476a3e106b516293162d8b88407af7f1c230d7

--- src/Dversion.java
+++ src/Dversion.java
@@ -11,18 +11,31 @@ import org.jsoup.select.Elements;
 public class Dversion {
 
 	public String getdv(String id) throws IOException {
-		
+
 		String dv = "Ê§°Ü";
+		String[] temp = new String[10]; 
 		String pageUrl = ""+id+"";
 		Document doc = Jsoup.connect(pageUrl).userAgent("Mozilla").get();
-		Elements e1 = doc.select("span[class]");
-		for (Iterator iterator = e1.iterator(); iterator.hasNext();) {
-			Element element = (Element) iterator.next();
-            if (element.attr("class").toString().equals("ov")) {
-    			dv= element.text().toString();
-			};		
+		Elements e1 = doc.select("div[data-gather]");
+		Elements e2 = doc.select("span[class]");
+		for (Iterator iterator1 = e1.iterator(); iterator1.hasNext();) {
+			Element element1 = (Element) iterator1.next();
+			if (element1.attr("data-gather").toString().contains(id)) {
+				temp = element1.text().toString().split("\\|");
+				dv=temp[1].substring(2, temp[1].length()-1);
+			}
+
+		}	
+		if (dv.equals("Ê§°Ü")) {
+			for (Iterator iterator2 = e2.iterator(); iterator2.hasNext();) {
+				Element element2 = (Element) iterator2.next();
+				if (element2.attr("class").toString().equals("ov")) {
+					dv= element2.text().toString();
+				};
+			}
 		}
 		return dv;
 	}
-	
+
 }
+
